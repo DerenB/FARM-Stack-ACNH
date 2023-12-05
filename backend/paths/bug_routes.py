94ec8403d6fdd89b11f6600226205a7a4e3d40f8
DerenB <dbozerprogramming@gmail.com>
2023-12-05 01:19:35 -0500

    Read bugs by title route

--- backend/paths/bug_routes.py
+++ backend/paths/bug_routes.py
@@ -6,7 +6,8 @@ router = APIRouter()
 
 # Import Database Functions
 from database_functions.bug_db_functions import (
-    read_all_bugs
+    read_all_bugs,
+    read_bugs_by_name
 )
 
 
@@ -15,3 +16,12 @@ from database_functions.bug_db_functions import (
 async def get_all_bugs():
     response = await read_all_bugs()
     return response
+
+
+# Read Bugs by Title
+@router.get("/bugs/{title}", tags=["bugs"])
+async def get_bugs_by_title(title):
+    response = await read_bugs_by_name(title)
+    if response:
+        return response
+    raise HTTPException(404, f"There are no bugs with this title: {title}")
