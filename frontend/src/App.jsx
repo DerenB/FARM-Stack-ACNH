680e35f1cc3e853e49ab9fa55fceb16ababec8f1
DerenB <dbozerprogramming@gmail.com>
2023-12-14 00:57:27 -0500

    App Browser Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import './styles/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { Bugs, Home, Notfound } from './pages';
 
 function App() {
   return (
     <div>
-      <p className='underline text-4xl'>Hello TailWind</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/bugs" element={<Bugs />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
