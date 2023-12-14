82e8a54c83da1f3116dbdb0347bf79fa33fe1854
DerenB <dbozerprogramming@gmail.com>
2023-12-14 00:56:38 -0500

    Home page basic

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import { Navbar } from '../components';
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+        <div>line</div>
+    </div>
+  )
+}
+
+export default Home
