7470371e73463cfa600c40338e1a126a512b5177
DerenB <dbozerprogramming@gmail.com>
2023-12-14 00:56:45 -0500

    Bugs page basic

---
+++ frontend/src/pages/Bugs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Navbar } from '../components';
+
+const Bugs = () => {
+  return (
+    <div>
+      <Navbar />
+      Bugs
+    </div>
+  )
+}
+
+export default Bugs
